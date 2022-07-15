68d9888aa255d88a37010f8cd8a21f4748d15d05
Phuong Bui <buiphuongaptutc@gmail.com>
2022-07-15 18:27:25 +0700

    tests

---
+++ src/Tester.java
@@ -0,0 +1,10 @@
+import models.Customer;
+
+public class Tester {
+    public static void main(String[] args) {
+        Customer customer = new Customer("first", "second", "j@domain.com");
+        System.out.println(customer);
+
+        customer = new Customer("first", "second", "email");
+    }
+}
